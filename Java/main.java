8123e3032783480e17f1d037d5043468bf9c93c8
Gersom Hernandez Ramos <gersomhernandez@me.com>
2023-02-23 20:36:01 -0600

    :art: Utilizando la clase Car
    Se utiliza la clase en dos instancias y se crea un metodo para la reutilizacion de la impresion

--- Java/main.java
+++ Java/main.java
@@ -2,5 +2,16 @@ package Java;
 class Main {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
+    Car car = new Car();
+    car.license = "AMQ123";
+    car.driver = "Andres Gomez";
+    car.passenger = 4;
+    car.printDataCar();
+
+    Car car2 = new Car();
+    car2.license = "QWE123";
+    car2.driver = "Andrea Gomez";
+    car2.passenger = 2;
+    car2.printDataCar();
   }
 }
