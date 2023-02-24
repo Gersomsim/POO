34abd26fdb948c02591594b262da5c00541b9ef5
Gersom Hernandez Ramos <gersomhernandez@me.com>
2023-02-23 23:55:19 -0600

    :thread: Encapsulamiento de propiedades

--- Java/Car.java
+++ Java/Car.java
@@ -4,14 +4,20 @@ public class Car {
   Integer id;
   String license;
   Driver driver;
-  Integer passenger;
+  private Integer passenger;
 
   public Car( String license, Driver driver ){
     this.license = license;
     this.driver = driver;
   }
+  public Integer getPassenger() {
+    return passenger;
+  }
+  public void setPassenger(Integer passenger) {
+    this.passenger = passenger;
+  }
 
   void printDataCar(){
-    System.out.println("License: " + this.license + " Name Driver: " + this.driver.name);
+    System.out.println("License: " + this.license + " Name Driver: " + this.driver.name + " Passenger: " + passenger);
   }
 }
--- Java/main.java
+++ Java/main.java
@@ -3,8 +3,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
     UberX uberX = new UberX("AMQ123", new Driver("Juan Perez", "JP21234", "j.perez@uber.com", "123ASD"), "Dodge", "Atitude");
-
-    uberX.passenger = 4;
+    uberX.setPassenger(4);
     uberX.printDataCar();
   }
 }
