bc0d8482bf12b6646864b9c3a9f12302d7a63248
Gersom Hernandez Ramos <gersomhernandez@me.com>
2023-02-23 20:40:25 -0600

    :art: Uso de la clase Car

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola mundo")
+    car = Car()
+    car.license = "AWS324"
+    car.driver = "Andres Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "AZU324"
+    car2.driver = "Martha Lopez"
+    print(vars(car2))
