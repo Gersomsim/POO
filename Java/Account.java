4e9f3bea2a1f25a1c6347db8c4c8bfae8e59d933
Gersom Hernandez Ramos <gersomhernandez@me.com>
2023-02-23 23:34:44 -0600

    :thread: Extends a Driver y user de account

--- Java/Account.java
+++ Java/Account.java
@@ -7,8 +7,14 @@ public class Account {
   String email;
   String password;
 
-  public Account( String name, String document ){
-    this.name = name;
+  public Account( String name, String document, String email, String password ){
+    this.name     = name;
     this.document = document;
+    this.email    = email;
+    this.password = password;
+  }
+
+  public void printDataAcount(){
+    System.out.println("Name: " + name + " document: " + document);
   }
 }
